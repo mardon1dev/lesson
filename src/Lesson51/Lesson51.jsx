b2afbc583794366f7ff80fdf4180755f89a01384
mardon1dev <dusbekovmardonbek5430@gmail.com>
2023-03-25 14:26:26 +0500

    Country is added

--- src/Lesson51/Lesson51.jsx
+++ src/Lesson51/Lesson51.jsx
@@ -1,159 +1,133 @@
-// import { useEffect } from "react";
-import { useState } from "react"
-import styles from "./lesson51.module.scss";
+// // import { useEffect } from "react";
+// import { useState } from "react"
+// // import styles from "./lesson51.module.scss";
 
 
-const Lesson51 = () => {
+// const Lesson51 = () => {
 
-  const url = `https://jsonplaceholder.typicode.com/users`;
+//   const [user, setUser] = useState(()=>{
+//     const getUser = localStorage.getItem("users");
+//     return getUser? JSON.parse(getUser): [];
+//   })
 
-  const getData = async ()=>{
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
-  }
+//   let [firstName, setFirstName] = useState("");
+//   let [lastName, setLastName] = useState("");
+//   let [userName, setUserName] = useState("");
+//   let [age, setAge]= useState("");
+//   let [salary, setSalary] = useState("");
 
-
-  const [user, setUser] = useState(()=>{
-    const getUser = localStorage.getItem("users");
-    return getUser? JSON.parse(getUser): [];
-  })
-
-  let [firstName, setFirstName] = useState("");
-  let [lastName, setLastName] = useState("");
-  let [userName, setUserName] = useState("");
-  let [age, setAge]= useState("");
-  let [salary, setSalary] = useState("");
-
-  const handleSetUser = (e)=>{
-    e.preventDefault();
-    firstName = document.querySelector("form").childNodes[0].value
-    lastName = document.querySelector("form").childNodes[1].value
-    userName = document.querySelector("form").childNodes[2].value
-    age = document.querySelector("form").childNodes[3].value
-    salary = document.querySelector("form").childNodes[4].value
-
-    if (id) {
-      document.querySelector(".col").childNodes[0].textContent = document.querySelector("form").childNodes[0].value
-      document.querySelector(".col").childNodes[1].textContent = document.querySelector("form").childNodes[1].value
-      document.querySelector(".col").childNodes[2].textContent = document.querySelector("form").childNodes[2].value
-      document.querySelector(".col").childNodes[3].textContent = document.querySelector("form").childNodes[3].value
-      document.querySelector(".col").childNodes[4].textContent = document.querySelector("form").childNodes[4].value
-    }
-    else{
-      if(!firstName.trim() && !lastName.trim() && !userName.trim() && !age.trim() && !salary.trim()) return;
-      setUser([...user, {
-        id: Date.now(),
-        fname: firstName,
-        lname: lastName,
-        uname: userName,
-        age: age,
-        salary: salary
-      },]);
-    }
-    
+//   const [editFirstName, setEditFirstName] = useState();
+//   const [editLastName, setEditLastName] = useState();
+//   const [editUserName, setEditUserName] = useState();
+//   const [editAge, setEditAge] = useState();
+//   const [editSalary, setEditSalary] = useState();
 
 
+//   const handleSetUser = (e)=>{
+//     e.preventDefault();
+//       if(!firstName.trim() && !lastName.trim() && !userName.trim() && !age.trim() && !salary.trim()) return;
 
-    
-    document.querySelector("form").childNodes[0].value = ""
-    document.querySelector("form").childNodes[1].value = ""
-    document.querySelector("form").childNodes[2].value = ""
-    document.querySelector("form").childNodes[3].value = ""
-    document.querySelector("form").childNodes[4].value = ""
-    setFirstName("");
-    setLastName("");
-    setUserName("");
-    setAge("");
-    setSalary("");
+//       if (editFirstName !== null && editLastName !== null && editUserName !== null && editAge !== null && editSalary !== null ) {
+//         const updateUser = user.map((user)=>{
+//           if (user.id === id) {
+//             return {...user, 
+//               fname: firstName,
+//               lname: lastName,
+//               uname: userName,
+//               age: age,
+//               salary: salary
+//             }
+//           }
+//           else{
+//             return user;
+//           }
+//         })
+//         setUser(updateUser);
+//         setEditFirstName(editFirstName)
+//         setEditLastName(editLastName)
+//         setEditUserName(editUserName)
+//         setEditAge(editAge)
+//         setEditSalary(editSalary)
 
-  }
-  localStorage.setItem("users", JSON.stringify(user));
-
-  const handleClear = ()=>{
-    localStorage.clear("users");
-    document.querySelector(".allusers").remove();
-    window.location.reload();
-  }
-
-  const handleDeleteUser = (id)=>{
-    const deleteUser = user.filter((user1)=> user1.id !== id);
-    setUser(deleteUser);
-  }
-
-  const [button, setButton] = useState(false);
-  const pressButton = ()=>{
-    setButton(!button)
-  }
+//         setEditFirstName("")
+//         setEditLastName("")
+//         setEditUserName("")
+//         setEditAge("")
+//         setEditSalary("")
 
 
-  const handleEditUser = (id)=>{
-    document.querySelector("form").childNodes[0].value = document.querySelector(".col").childNodes[0].textContent;
-    document.querySelector("form").childNodes[1].value = document.querySelector(".col").childNodes[1].textContent;
-    document.querySelector("form").childNodes[2].value = document.querySelector(".col").childNodes[2].textContent;
-    document.querySelector("form").childNodes[3].value = document.querySelector(".col").childNodes[3].textContent;
-    document.querySelector("form").childNodes[4].value = document.querySelector(".col").childNodes[4].textContent; 
-    const editFirstName = document.querySelector("form").childNodes[0].value;
-    const editLastName = document.querySelector("form").childNodes[1].value;
-    const editUserName = document.querySelector("form").childNodes[2].value;
-    const editAge = document.querySelector("form").childNodes[3].value;
-    const editSalary = document.querySelector("form").childNodes[4].value;
+//       }
+//       else{
+//         setUser([...user, {
+//           id: Date.now(),
+//           fname: firstName,
+//           lname: lastName,
+//           uname: userName,
+//           age: age,
+//           salary: salary
+//         },]);
+//       setFirstName("");
+//       setLastName("");
+//       setUserName("");
+//       setAge("");
+//       setSalary("");
+//       }
 
-    if (user.id === id) {
-      
-    }
-    console.log(editFirstName, editLastName, editUserName, editAge, editSalary);
-    // console.log(buttonedit);
-    let data = JSON.parse(localStorage.getItem("users"));
-    console.log(data);
-    const editdetName =  data.map(person=>{
-      if (person.id === id) {
-        return{
-          ...person, 
-          fname:  editFirstName,
-          lname: editLastName,
-          uname: editUserName,
-          age: editAge,
-          salary: editSalary  
-        }
-      }
-      console.log(person);
-      return person;
-    })
-    console.log(editdetName);
-    localStorage.setItem("users", JSON.stringify(editdetName));
+//   }
+//   localStorage.setItem("users", JSON.stringify(user));
 
-  }
+//   const handleClear = ()=>{
+//     localStorage.clear("users");
+//     document.querySelector(".allusers").remove();
+//     window.location.reload();
+//   }
 
-  return (
-    <div className="container">
-      <button className="btn btn-primary btn1 mt-5 mb-5" onClick={pressButton}>Add user</button>
-      <form onSubmit={handleSetUser} className={button? `d-flex`: `d-none`}> 
-        <input type="text"  onChange={(e)=>setFirstName(e.target.value)}  placeholder="Firstname"/>
-        <input type="text"  onChange={(e)=>setLastName(e.target.value)} />
-        <input type="text"  onChange={(e)=>setUserName(e.target.value)} />
-        <input type="number" onChange={(e)=>{setAge(e.target.value)}}/>
-        <input type="number" onChange={(e) =>{setSalary(e.target.value)}} />
-        <button type="submit" className="btn btn-primary">Add</button>
-      </form>
-        <div className="allusers">
-        {
-              user.map((user)=>(
-                <div key={user.id} className="d-flex col">
-                  <h1>{user.fname}</h1>
-                  <h2>{user.lname}</h2>
-                  <h3>{user.uname}</h3>
-                  <h3>{user.age}</h3>
-                  <h4>{user.salary}</h4>
-                  <button onClick={()=>{handleDeleteUser(user.id)}}>Delete</button>
-                  <button onClick={()=>{handleEditUser(user.id)}}>Edit</button> 
-                </div>
-              ))
-            }
-        </div>
-            <button onClick={handleClear} className="btn btn-danger mt-5 mb-5">Clear all</button>
-    </div>
-  )
-}
+//   const handleDeleteUser = (id)=>{
+//     const deleteUser = user.filter((user1)=> user1.id !== id);
+//     setUser(deleteUser);
+//   }
 
-export default Lesson51;
+//   const [button, setButton] = useState(false);
+//   const pressButton = ()=>{
+//     setButton(!button)
+//   }
+
+
+//   const handleEditUser = (id)=>{
+//     const  userEdit = user.find((user)=>user.id === id);
+//     setEditFirstName(id);
+//     setFirstName(userEdit.text)
+//   }
+
+//   return (
+//     <div className="container">
+//       <button className="btn btn-primary btn1 mt-5 mb-5" onClick={pressButton}>Add user</button>
+//       <form onSubmit={handleSetUser} className={button? `d-flex`: `d-none`}> 
+//         <input type="text" value={firstName}  onChange={(e)=>setFirstName(e.target.value)}  placeholder="Firstname"/>
+//         <input type="text" value={lastName}  onChange={(e)=>setLastName(e.target.value)} />
+//         <input type="text" value={userName}  onChange={(e)=>setUserName(e.target.value)} />
+//         <input type="number" value={age} onChange={(e)=>{setAge(e.target.value)}}/>
+//         <input type="number" value={salary} onChange={(e) =>{setSalary(e.target.value)}} />
+//         <button type="submit" className="btn btn-primary">Add</button>
+//       </form>
+//         <div className="allusers">
+//         {
+//               user.map((user)=>(
+//                 <div key={user.id} className="d-flex col">
+//                   <h1>{user.fname}</h1>
+//                   <h2>{user.lname}</h2>
+//                   <h3>{user.uname}</h3>
+//                   <h3>{user.age}</h3>
+//                   <h4>{user.salary}</h4>
+//                   <button onClick={()=>{handleDeleteUser(user.id)}}>Delete</button>
+//                   <button onClick={()=>{handleEditUser(user.id)}}>Edit</button> 
+//                 </div>
+//               ))
+//             }
+//         </div>
+//             <button onClick={handleClear} className="btn btn-danger mt-5 mb-5">Clear all</button>
+//     </div>
+//   )
+// }
+
+// export default Lesson51;
---
+++ src/Lesson52/Lesson52.jsx
@@ -0,0 +1,124 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import "./Lesson52.scss";
+import Find from './find2.png'
+
+const Lesson52 = () => {
+
+    const search = document.querySelector(".search");
+    const toggledInput = document.querySelector(".input");
+    const handleClick = () =>{
+        search.classList.toggle("active");
+        toggledInput.focus();   
+    };
+
+    const {t, i18n} = useTranslation();
+
+    const handleChangeLanguage = (lan)=>{
+      i18n.changeLanguage(lan);
+    }
+
+    const [country,  setCountry] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error,  setError] = useState(false);
+    const [input, setInput] = useState("");
+ 
+    const findCountry = (e)=>{
+        e.preventDefault();
+
+    }
+   
+    const getData = async ()=>{
+
+        let url;
+        if (input) {
+
+            url = `https://restcountries.com/v3.1/name/${input}?fulText=false`;
+        }
+        else{
+            url = `https://restcountries.com/v3.1/all`
+        }
+        try{  
+            const response = await axios.get(url);
+            setCountry(response.data);
+          }
+        catch{
+            setError(error);
+        }
+        finally{
+            setLoading(false);
+        }
+
+console.log(country);
+    }
+    useEffect(()=>{
+        getData();
+    }, [input]);
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
+  return (
+    <div>
+        <div className='container'>
+            <div>
+                <button className="button" onClick={()=>{handleChangeLanguage("eng")}}>Eng</button>
+                <button className="button" onClick={()=>{handleChangeLanguage("ru")}}>ru</button>
+
+                <h1>
+                    {
+                        t(`text`)
+                    }
+                </h1>
+            </div>
+            <div>
+            </div>
+            <div>
+                <form className="mt-5 mb-5 search" onSubmit={findCountry}>
+                    <input type="text input" onChange={(e)=>{setInput(e.target.value)}} placeholder="Search..." className="input"/>
+                    <button type="submit" className="btn" onClick={handleClick}><img src={Find} alt="search" /></button>
+                </form>
+                <div className="row d-flex justify-content-center align-items-center g-3">
+                    {
+
+                        country.slice(0,6).map((davlat, index)=>{
+                            const { flags, name, population, currencies, capital } = davlat;
+                            let populationFixed;
+                                if (population > 1000000) {
+                                    populationFixed = `${Math.floor(population/1000000).toFixed()} mln`
+                                } else if(population > 1000){
+                                    populationFixed = `${Math.floor(population/1000).toFixed()} ming`;
+                                } else if(population == 0){
+                                    populationFixed = `${population}`
+                                } 
+                                else{
+                                    populationFixed = `Axoli juda kam`
+                                }
+                                const currenciesInArray2 = Object.entries(currencies)[0][1].name;
+
+                            return <div className="col-12 col-md-6 col-lg-4  text-center" key={index}>
+                                         <div className="card border-0"> 
+                                         <div className="cart">
+                                            <img className="image" src={flags.svg} alt={flags.alt}/>
+                                            <div className="cart-body ">
+                                                <h2>{name.common}</h2>
+                                                <h4>{capital}</h4>
+                                                <span>Population: {populationFixed}</span>
+                                                <p>Currency: {currenciesInArray2}</p>
+                                            </div>
+                                        </div>
+                                         </div>
+                                    </div>
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Lesson52;
